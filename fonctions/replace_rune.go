f0df6d7edfea2f5509a9250a1ab1f394f95ca8b5
ahliko <killian.guillemot@ynov.com>
2022-10-17 16:35:51 +0200

    Update replace_rune.go

--- fonctions/replace_rune.go
+++ fonctions/replace_rune.go
@@ -1,6 +1,6 @@
 package hangman
 
-// Replace a rune in a slice of runes
+// remplacer une rune dans un tableau de rune
 func Replace_rune(s []rune, r rune, i int) []rune {
 	s[i] = r // remplacer le caractère à l'index i par le caractère r
 	return s // retourner le slice de runes
