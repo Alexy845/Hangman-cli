9c1dcfeb0b80d8f077f8d86cd38b5ea5b962f777
Manon-Arc <arcasmanon47@gmail.com>
2022-10-17 14:28:09 +0200

    add main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"hangman/fonction"
+	"log"
+	"os"
+)
+
+func main() {
+
+	switch len(os.Args) {
+	case 1:
+		fmt.Print("Please enter dictionary file")
+		return
+	case 2:
+		dico := os.Args[1]
+		if _, err := os.Stat(os.Args[1]); err != nil {
+			log.Fatal(err)
+		}
+		word := hangman.Word{The_word: hangman.Rand_word(dico), Word_runes: []rune{}, N: 0, Attempts: 10}
+		word.N = len(word.The_word)/2 - 1
+		word.Word_runes = make([]rune, len(word.The_word)-1)
+		word.Rand_letters()
+		word.Play()
+	default:
+		fmt.Print("Too many arguments !")
+	}
+}
