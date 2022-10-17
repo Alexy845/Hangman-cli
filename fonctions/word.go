79658703bfaae68005ef6b1f01dc8e3b210c9e02
ahliko <killian.guillemot@ynov.com>
2022-10-17 16:30:42 +0200

    Update word.go

--- fonctions/word.go
+++ fonctions/word.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-type Word struct {
+type Word struct { // Structure du mot à trouver
 	The_word         string
 	Word_runes       []rune
 	N                int
@@ -18,12 +18,12 @@ type Word struct {
 // Choose n random letters in a slice of runes
 func (w *Word) Rand_letters() {
 	var numbers int
-	for i := 0; i < w.N; i++ {
-		numbers = Rand_range(0, len(w.The_word)-1)
-		if w.Word_runes[numbers] == 0 {
-			w.Word_runes = Replace_rune(w.Word_runes, rune(w.The_word[numbers]), numbers)
+	for i := 0; i < w.N; i++ { // Choisi n lettres aléatoires
+		numbers = Rand_range(0, len(w.The_word)-1) // Choisi un nombre aléatoire
+		if w.Word_runes[numbers] == 0 {            // Si la lettre n'a pas déjà été choisie
+			w.Word_runes = Replace_rune(w.Word_runes, rune(w.The_word[numbers]), numbers) // Si la lettre n'est pas déjà dans le slice, on l'ajoute
 		} else {
-			i--
+			i-- // Si la lettre est déjà dans le slice, on recommence
 		}
 	}
 }
@@ -34,26 +34,26 @@ func (w *Word) Play() {
 	fmt.Println(strings.ToUpper(AfficheRune(w.Word_runes)))
 	fmt.Print("\n")
 	for {
-		if w.Attempts == 0 {
+		if w.Attempts == 0 { // Si le joueur n'a plus d'essais il perd
 			fmt.Println("You lost !")
 			fmt.Println("The word was", w.The_word)
 			break
-		} else if w.Check_win() {
+		} else if w.Check_win() { // Si le joueur a trouvé le mot il gagne
 			fmt.Println("Congrat !")
 			fmt.Println("The word was", w.The_word)
 			break
 		} else {
 			fmt.Println("Enter a letter")
 			var letter rune
-			input := bufio.NewScanner(os.Stdin) // créer un scanner sur l'entrée
-			input.Scan()                        // lance le scan
-			for _, i := range strings.ToLower(input.Text()) {
+			input := bufio.NewScanner(os.Stdin)               // Créer un scanner sur l'entrée
+			input.Scan()                                      // Lance le scan
+			for _, i := range strings.ToLower(input.Text()) { // Si la lettre est en majuscule, on la passe en minuscule
 				letter = i
 			}
 			fmt.Println("Choose : " + strings.ToUpper(input.Text()))
 			fmt.Print("\n")
-			w.Check_letter(letter)
-			fmt.Println(strings.ToUpper(AfficheRune(w.Word_runes)))
+			w.Check_letter(letter)                                  // Verifie si la lettre est dans le mot
+			fmt.Println(strings.ToUpper(AfficheRune(w.Word_runes))) // Affiche le mot avec les lettres trouvées
 			fmt.Print("\n")
 		}
 	}
