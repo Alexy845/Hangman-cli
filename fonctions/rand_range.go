822ba4a2aa500f8766bc0d268e4b64f59a04821b
ahliko <killian.guillemot@ynov.com>
2022-10-17 16:39:32 +0200

    Update rand_range.go (documentation)

--- fonctions/rand_range.go
+++ fonctions/rand_range.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-// Random number in a range
+// Nombre aléatoire dans un intervalle
 func Rand_range(min int, max int) int {
-    rand.Seed(time.Now().UnixNano())
-    return rand.Intn(max-min) + min
+	rand.Seed(time.Now().UnixNano()) // initialiser le générateur de nombres aléatoires
+	return rand.Intn(max-min) + min  // retourner un nombre aléatoire dans l'intervalle
 }
