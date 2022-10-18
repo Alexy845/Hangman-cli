d0015fe773a96a8244c9d8b93d89bfe7c3465d87
ahliko <killian.guillemot@ynov.com>
2022-10-18 10:16:02 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,20 +9,20 @@ import (
 
 func main() {
 	switch len(os.Args) { // Vérifie le nombre d'arguments passés au programme (doit être égal à 2)
-	case 1:
+	case 1: // Si il n'y a pas d'arguments
 		fmt.Print("Please enter dictionary file")
-		return
-	case 2:
+		return // On sort du programme
+	case 2: // Si il y a un argument
 		dico := os.Args[1] // Récupère le nom du fichier dictionnaire
 		if _, err := os.Stat(os.Args[1]); err != nil {
 			log.Fatal(err) // Vérifie que le fichier existe
 		}
 		word := hangman.Word{The_word: hangman.Rand_word(dico), Word_runes: []rune{}, N: 0, Attempts: 10} // Création de l'objet Word
 		word.N = len(word.The_word)/2 - 1                                                                 // Nombre de lettres à trouver
-		word.Word_runes = make([]rune, len(word.The_word)-1)                                              // Création d'un slice de rune de la taille du mot à trouver
+		word.Word_runes = make([]rune, len(word.The_word))                                                // Création d'un slice de rune de la taille du mot à trouver
 		word.Rand_letters()                                                                               // Rempli le tableau de runes avec des lettres aléatoires
 		word.Play()                                                                                       // Début du jeu
-	default:
+	default: // Si il y a plus d'un argument
 		fmt.Print("Too many arguments !")
 	}
 }
