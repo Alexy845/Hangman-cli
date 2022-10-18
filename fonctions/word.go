d5a6ef2aef9908487376f2919edfa230a8c8fbfc
ahliko <killian.guillemot@ynov.com>
2022-10-18 14:56:39 +0200

    Add rand_letters.go file

--- fonctions/word.go
+++ fonctions/word.go
@@ -17,19 +17,6 @@ type Word struct { // Structure du mot à trouver
 	Letter_used      []rune
 }
 
-// Choose n random letters in a slice of runes
-func (w *Word) Rand_letters() {
-	var numbers int
-	for i := 0; i < w.N; i++ { // Choisi n lettres aléatoires
-		numbers = Rand_range(0, len(w.The_word)-1) // Choisi un nombre aléatoire
-		if w.Word_runes[numbers] == 0 {            // Si la lettre n'a pas déjà été choisie
-			w.Word_runes = Replace_rune(w.Word_runes, rune(w.The_word[numbers]), numbers) // Si la lettre n'est pas déjà dans le slice, on l'ajoute
-		} else {
-			i-- // Si la lettre est déjà dans le slice, on recommence
-		}
-	}
-}
-
 // infinite loop to play the game until the player wins or loses
 func (w *Word) Play() {
 	fmt.Println("Good Luck, you have 10 attempts.")
