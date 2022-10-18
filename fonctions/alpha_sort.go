4b320cdd5f775314c6c0427144919bb96402f0ae
Manon-Arc <arcasmanon47@gmail.com>
2022-10-18 16:08:31 +0200

    add bonus used letter

---
+++ fonctions/alpha_sort.go
@@ -0,0 +1,12 @@
+package hangman
+
+func AlphaSort(str []rune,) {
+    for x := range str {
+        y := x + 1
+        for y = range str {
+            if str[x] < str[y] {
+                str[x], str[y] = str[y], str[x]
+            }
+        }
+    }
+}
--- fonctions/word.go
+++ fonctions/word.go
@@ -41,15 +41,20 @@ func (w *Word) Play() {
 			fmt.Print("\n")
 			if len(input.Text()) == 1 { // Si la l'entrée est une lettre
 				letter = rune(strings.ToLower(input.Text())[0]) // Converti la lettre en rune
-				w.Check_letter(letter)                          // Verifie si la lettre est dans le mot
-				w.Letter_used = append(w.Letter_used, letter)   // On ajoute la lettre dans le slice des lettres utilisées
+				AlphaSort(w.Letter_used)
+				if w.Is_used(letter) {       			
+					fmt.Println(strings.ToUpper(input.Text()) + " is already used like : " + strings.ToUpper(string(w.Letter_used)))
+				} else {
+				w.Letter_used = append(w.Letter_used, (letter)) // On ajoute la lettre dans le slice des lettres utilisées
+				}
+				w.Check_letter(letter)                        // Verifie si la lettre est dans le mot				
 			} else if len(input.Text()) > 1 { // Si la longueur de l'entrée est supérieur à 1
 				if w.Check_word(strings.ToLower(input.Text())) { // Verifie si le mot est le bon
 					fmt.Println("Congrat !")
 					fmt.Println("The word was", w.The_word)
 					return // Si le mot est bon, on sort de la boucle
-				} else { // Sinon on perd une vie
-					w.Attempts -= 2 // On perd 2 vies
+				} else { 
+					w.Attempts -= 2 // sinon perd 2 vies
 					fmt.Println("Not present in the word, " + strconv.Itoa(w.Attempts) + " attemps remaining")
 					Affiche(9 - w.Attempts) // On affiche le pendu
 				}
