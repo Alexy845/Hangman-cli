e710050308d2662094d721ad65778ea8b17a6500
ahliko <killian.guillemot@ynov.com>
2022-10-18 10:11:55 +0200

    Update check_letter.go (documentation)

--- fonctions/check_letter.go
+++ fonctions/check_letter.go
@@ -1,22 +1,22 @@
 package hangman
 
 import (
-	"strings"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-// Check if the letter is in the word
-func (w *Word)Check_letter(letter rune) {
-	if strings.ContainsRune(w.The_word, letter) {
-		for i := 0; i < len(w.The_word)-1; i++ {
-			if rune(w.The_word[i]) == letter {
-				w.Word_runes = Replace_rune(w.Word_runes, letter, i)
+// Vérifie si la lettre est dans le mot
+func (w *Word) Check_letter(letter rune) {
+	if strings.ContainsRune(w.The_word, letter) { // Si la lettre est dans le mot
+		for i := 0; i < len(w.The_word)-1; i++ { // Pour chaque lettre du mot
+			if rune(w.The_word[i]) == letter { // Si la lettre est égale à la lettre entrée
+				w.Word_runes = Replace_rune(w.Word_runes, letter, i) // On remplace la lettre dans le slice
 			}
 		}
-	} else {
-		w.Attempts--
+	} else { // Si la lettre n'est pas dans le mot
+		w.Attempts-- // On perd une vie
 		fmt.Println("Not present in the word, " + strconv.Itoa(w.Attempts) + " attemps remaining")
-		Affiche(9-w.Attempts)
+		Affiche(9 - w.Attempts) // On affiche le pendu
 	}
 }
