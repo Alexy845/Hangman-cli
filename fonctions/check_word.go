d45a1b69f32dcf83b5132cc3488ac460e6ca8651
ahliko <killian.guillemot@ynov.com>
2022-10-18 09:50:24 +0200

    Update check_word.go (documentation

--- fonctions/check_word.go
+++ fonctions/check_word.go
@@ -1,11 +1,11 @@
 package hangman
 
-// Check if the word is right
+// Vérifie si le mot est trouvé
 func (w *Word) Check_word(input string) bool {
-	for i := 0; i < len(input)-1; i++ {
-		if w.Word_runes[i] != rune(input[i]) {
-			return false
+	for i := 0; i < len(input)-1; i++ { // Pour chaque lettre du mot entré
+		if w.Word_runes[i] != rune(input[i]) { // Si la lettre n'est pas dans le mot
+			return false // On retourne faux
 		}
 	}
-	return true
+	return true // Si toutes les lettres sont dans le mot, on retourne vrai
 }
