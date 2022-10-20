636e3ee4fa5020d20a29cd4d8387c5768f7634aa
Manon-Arc <arcasmanon47@gmail.com>
2022-10-20 15:09:34 +0200

    modif save.go

--- fonctions/save.go
+++ fonctions/save.go
@@ -14,9 +14,11 @@ func Save(w Word) {
 	}
 	objects_save := w
 	save, err := json.Marshal(objects_save)
-     
     if err != nil {
         fmt.Println(err)
     } 
 	err = ioutil.WriteFile(".\\asset\\save.txt", save, 0600)
+	if err != nil {
+        fmt.Println(err)
+    } 
 }
