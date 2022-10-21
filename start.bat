95105d59cbfd9a78dc8c1bf67cf5c7ef254d6bf9
ahliko <killian.guillemot@ynov.com>
2022-10-21 15:57:35 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,34 +1,35 @@
-#!/bin/sh -x
 @echo off
 
-start_mode_normal() {
-    echo "Starting in normal mode"
-    # Start the application
-    go run main.go asset/words.txt
-}
-
-start_mode_hard (){
-    echo "Starting in hard mode"
-    # Start the application
-    go run main.go --hard asset/words.txt
-}
-
-start_with_save (){
-    echo "Starting with save"
-    # Start the application
-    go run main.go --startWith save.txt
-}
 echo "___________________________"
 echo "Choose a mode:            |"
 echo "1. Normal                 |"
 echo "2. Hard                   |"
 echo "3. Start with save        |"
 echo "___________________________"
-read -p "Enter your choice: " choice
+read "-p" "Enter your choice: " "choice"
+IF "%choice%"=="1" (
+  CALL :start_mode_normal
+) ELSE IF "%choice%"=="2" (
+  CALL :start_mode_hard
+) ELSE IF "%choice%"=="3" (
+  CALL :start_with_save
+) ELSE (
+  echo "Invalid choice"
+)
 
-case $choice in
-    1) start_mode_normal ;;
-    2) start_mode_hard ;;
-    3) start_with_save ;;
-    *) echo "Invalid choice" ;;
-esac
+EXIT /B %ERRORLEVEL%
+
+:start_mode_normal
+echo "Starting in normal mode"
+go "run" "main.go" "asset\words.txt"
+EXIT /B 0
+
+:start_mode_hard
+echo "Starting in hard mode"
+go "run" "main.go" "--hard" "asset\words.txt"
+EXIT /B 0
+
+:start_with_save
+echo "Starting with save"
+go "run" "main.go" "--startWith" "save.txt"
+EXIT /B 0
