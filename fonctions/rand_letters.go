9f3e665ac187291decff5776f7541d846d38f44b
gkillian <killian.guillemot@ynov.com>
2022-11-15 12:13:08 +0100

    Update files

--- fonctions/rand_letters.go
+++ fonctions/rand_letters.go
@@ -1,9 +1,12 @@
 package hangman
 
-import "strings"
+import (
+	"strings"
+)
 
 // Choose n random letters from the slice of runes to reveal
 func (w *Word) Rand_letters() {
+
 	var numbers int
 	for i := 0; i < w.N; i++ {
 		numbers = Rand_range(0, len(w.The_word)-1)
--- fonctions/word.go
+++ fonctions/word.go
@@ -1,5 +1,9 @@
 package hangman
 
+import (
+	"fmt"
+)
+
 type Word struct { // Struct of the word
 	The_word         string
 	Word_runes       []rune
@@ -10,4 +14,18 @@ type Word struct { // Struct of the word
 	Alpha_letter     string
 	Hard             bool
 	Voyelle_used     []rune
+}
+
+func (ww *Word) Start() {
+
+	fmt.Println("Good Luck, you have 10 attempts.")
+
+	ww.N = len(ww.The_word)/2 - 1
+	ww.Word_runes = make([]rune, len(ww.The_word))
+	ww.Rand_letters()
+	println(ww.The_word)
+
+	/*default: // If there is 3 args or more
+		fmt.Print("Too many arguments !")
+	}*/
 }
